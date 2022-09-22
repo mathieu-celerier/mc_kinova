fca02e17ef3271b3e809fb98f6f7dbb80f7a9527
Mathieu Celerier <mathieu.celerier@aist.go.jp>
2022-09-22 10:45:25 +0900

    Add convex shapes first kinematics simulations OK

--- src/KinovaRobotModule.in.cpp
+++ src/KinovaRobotModule.in.cpp
@@ -9,22 +9,23 @@ namespace
 {
 
 // This is set by CMake, see CMakeLists.txt
-static const std::string JVRC_DESCRIPTION_PATH = "@JVRC_DESCRIPTION_PATH@";
+static const std::string KINOVA_DESCRIPTION_PATH = "@KORTEX_DESCRIPTION_PATH@";
 
 } // namespace
 
 namespace mc_robots
 {
 
-KinovaRobotModule::KinovaRobotModule() : mc_rbdyn::RobotModule(JVRC_DESCRIPTION_PATH, "jvrc1")
+KinovaRobotModule::KinovaRobotModule() : mc_rbdyn::RobotModule(KINOVA_DESCRIPTION_PATH, "kinova")
 {
-  // True if the robot has a fixed base, false otherwise
-  bool fixed = false;
+  mc_rtc::log::success("KinovaRobotModule loaded with name: {}", name);
+  urdf_path = path + "/arms/gen3/7dof/urdf/GEN3_URDF_V12.urdf";
+  _real_urdf = urdf_path;
   // Makes all the basic initialization that can be done from an URDF file
-  init(rbd::parsers::from_urdf_file(urdf_path, fixed));
+  init(rbd::parsers::from_urdf_file(urdf_path, true));
 
   // Automatically load the convex hulls associated to each body
-  std::string convexPath = path + "/convex/" + name + "/";
+  std::string convexPath = "@PROJECT_SOURCE_DIR@/convex/" + name + "/";
   bfs::path p(convexPath);
   if(bfs::exists(p) && bfs::is_directory(p))
   {
@@ -43,52 +44,55 @@ KinovaRobotModule::KinovaRobotModule() : mc_rbdyn::RobotModule(JVRC_DESCRIPTION_
   }
 
   // Define some force sensors
-  _forceSensors.push_back(mc_rbdyn::ForceSensor("RightFootForceSensor", "R_ANKLE_P_S", sva::PTransformd::Identity()));
-  _forceSensors.push_back(mc_rbdyn::ForceSensor("LeftFootForceSensor", "L_ANKLE_P_S", sva::PTransformd::Identity()));
-  _forceSensors.push_back(mc_rbdyn::ForceSensor("RightHandForceSensor", "R_WRIST_Y_S", sva::PTransformd::Identity()));
-  _forceSensors.push_back(mc_rbdyn::ForceSensor("LeftHandForceSensor", "L_WRIST_Y_S", sva::PTransformd::Identity()));
+  _forceSensors.push_back(mc_rbdyn::ForceSensor("EEForceSensor", "EndEffector_Link", sva::PTransformd::Identity()));
 
-  // Define body sensors
-  _bodySensors.emplace_back("Accelerometer", "PELVIS_S", sva::PTransformd(Eigen::Vector3d(-0.0325, 0, 0.1095)));
-  _bodySensors.emplace_back("FloatingBase", "PELVIS_S", sva::PTransformd::Identity());
+  // Clear body sensors
+  _bodySensors.clear();
 
+  const double i = 0.03;
+  const double s = 0.015;
+  const double d = 0.;
   // Define a minimal set of self-collisions
   _minimalSelfCollisions = {
-      {"WAIST_R_S", "L_SHOULDER_Y_S", 0.02, 0.001, 0.}, {"WAIST_R_S", "R_SHOULDER_Y_S", 0.02, 0.001, 0.},
-      {"PELVIS_S", "R_ELBOW_P_S", 0.05, 0.001, 0.},     {"PELVIS_S", "L_ELBOW_P_S", 0.05, 0.001, 0.},
-      {"R_WRIST_Y_S", "R_HIP_Y_S", 0.05, 0.025, 0.},    {"L_WRIST_Y_S", "L_HIP_Y_S", 0.05, 0.025, 0.}};
+      {"base_link", "spherical_wrist_1_link", i, s, d},
+      {"shoulder_link", "spherical_wrist_1_link", i, s, d},
+      {"half_arm_1_link", "spherical_wrist_1_link", i, s, d},
+      {"half_arm_2_link", "spherical_wrist_1_link", i, s, d},
+      {"base_link", "spherical_wrist_2_link", i, s, d},
+      {"shoulder_link", "spherical_wrist_2_link", i, s, d},
+      {"half_arm_1_link", "spherical_wrist_2_link", i, s, d},
+      {"half_arm_2_link", "spherical_wrist_2_link", i, s, d},
+      {"base_link", "bracelet_link", i, s, d},
+      {"shoulder_link", "bracelet_link", i, s, d},
+      {"half_arm_1_link", "bracelet_link", i, s, d},
+      {"half_arm_2_link", "bracelet_link", i, s, d}
+  };
+
+  /* Additional self collisions */
+
   _commonSelfCollisions = _minimalSelfCollisions;
 
   // Define simple grippers
-  _grippers = {{"l_gripper", {"L_UTHUMB"}, true}, {"r_gripper", {"R_UTHUMB"}, false}};
+  // _grippers = {{"l_gripper", {"L_UTHUMB"}, true}, {"r_gripper", {"R_UTHUMB"}, false}};
 
   // Default configuration of the floating base
-  _default_attitude = {{1., 0., 0., 0., 0., 0., 0.8275}};
+  _default_attitude = {{1., 0., 0., 0., 0., 0., 0.0}};
 
   // Default joint configuration, if a joint is omitted the configuration is 0 or the middle point of the limit range if
   // 0 is not a valid configuration
-  _stance["L_ANKLE_P"] = {-0.33};
-  _stance["L_ANKLE_R"] = {-0.02};
-  _stance["L_ELBOW_P"] = {-0.52};
-  _stance["L_HIP_P"] = {-0.38};
-  _stance["L_HIP_R"] = {0.02};
-  _stance["L_KNEE"] = {0.72};
-  _stance["L_SHOULDER_P"] = {-0.052};
-  _stance["L_SHOULDER_R"] = {0.17};
-  _stance["L_SHOULDER_Y"] = {0.0};
-  _stance["R_ANKLE_P"] = {-0.33};
-  _stance["R_ANKLE_R"] = {-0.01};
-  _stance["R_ELBOW_P"] = {-0.52};
-  _stance["R_HIP_P"] = {-0.38};
-  _stance["R_HIP_R"] = {-0.01};
-  _stance["R_KNEE"] = {0.72};
-  _stance["R_SHOULDER_P"] = {-0.052};
-  _stance["R_SHOULDER_R"] = {-0.17};
-  _stance["WAIST_P"] = {0.13};
+  _stance["Actuator1"] = {-1.57};
+  _stance["Actuator2"] = {-0.35};
+  _stance["Actuator3"] = {3.14};
+  _stance["Actuator4"] = {-2.0};
+  _stance["Actuator5"] = {0.0};
+  _stance["Actuator6"] = {-1.0};
+  _stance["Actuator7"] = {1.57};
+
+  mc_rtc::log::success("PandaRobotModule uses urdf_path {}", urdf_path);
 }
 
 } // namespace mc_robots
 
 #include <mc_rbdyn/RobotModuleMacros.h>
 
-ROBOT_MODULE_DEFAULT_CONSTRUCTOR("KinovaRobotModule", mc_robots::KinovaRobotModule)
+ROBOT_MODULE_DEFAULT_CONSTRUCTOR("kinova", mc_robots::KinovaRobotModule)
