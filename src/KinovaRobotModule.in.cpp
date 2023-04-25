9064941fc69238e38044fa8bdae89653907648aa
Mathieu Celerier <mathieu.celerier@aist.go.jp>
2023-04-25 22:12:57 +0900

    Updated frame for FT sensor

--- src/KinovaRobotModule.in.cpp
+++ src/KinovaRobotModule.in.cpp
@@ -114,7 +114,7 @@ KinovaRobotModule::KinovaRobotModule() : mc_rbdyn::RobotModule(KINOVA_DESCRIPTIO
   }
 
   // Define a force sensor
-  _forceSensors.push_back(mc_rbdyn::ForceSensor("EEForceSensor", "end_effector_link", sva::PTransformd::Identity()));
+  _forceSensors.push_back(mc_rbdyn::ForceSensor("EEForceSensor", "FT_frame", sva::PTransformd::Identity()));
 
   // Define a device sensor for external torque measurment
   _devices.push_back(mc_rbdyn::ExternalTorqueSensor("externalTorqueSensor", 7).clone());
