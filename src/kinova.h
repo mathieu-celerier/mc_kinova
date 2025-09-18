4173522455d2ceb177739ccb824aa142b87054b4
ThomasDuvinage <toto.duvinage@gmail.com>
2025-09-18 10:42:14 +0900

    feat(VirtualTorqueSensor): comment code, to be reverted when VirtualTorqueSensor is added to mc_rtc

--- src/kinova.h
+++ src/kinova.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <mc_rbdyn/RobotModule.h>
-#include <mc_rbdyn/VirtualTorqueSensor.h>
+
+// TODO To be reset after new feature is added
+// #include <mc_rbdyn/VirtualTorqueSensor.h>
 
 #include <mc_robots/api.h>
 
